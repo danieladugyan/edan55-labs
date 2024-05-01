f8627f4b70ea0e886031c7d98bf9339c64fed601
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-05-01 14:04:03 +0200

    without delete

--- independentset/main.py
+++ independentset/main.py
@@ -1,6 +1,25 @@
+from re import I
+from time import sleep
+
 import numpy as np
 
-FILE_PATH = "./data/g4.in"
+FILE_PATH = "./data/g30.in"
+np.set_printoptions(linewidth=np.inf)
+
+
+def find_vertex_without_neighbours(graph: np.ndarray):
+    vertices = np.where(np.all(graph <= 0, axis=1))[0]
+    if vertices.size > 0:
+        vertices = np.where(np.count_nonzero(graph[vertices] == 0, axis=0))[0]
+        if vertices.size > 0:
+            vertex = vertices[0].item()
+            # print("lone vertex = ", vertex, graph)
+            return vertex
+    return None
+
+
+def find_vertex_with_max_degree(graph: np.ndarray):
+    return np.argmax(np.count_nonzero(graph > 0, axis=0))
 
 
 def r0(graph: np.ndarray) -> int:
@@ -9,36 +28,34 @@ def r0(graph: np.ndarray) -> int:
     If the input graph G has a vertex v without any neighbors, return 1 + R0(G[V - v]).
     Otherwise find a vertex u of maximum degree and return max(1 + R0(G[V - N[u]]), R0(G[V - u]))
     """
-    print("-"*10)
+    print("-" * 10)
     print(graph)
+    sleep(1)
     if np.all(graph == -1):
         return 0
 
     # Find row where all elements are equal to 0
-    v = np.where(np.all(graph == 0, axis=1))[0]
-    print("v = ", v)
-    if v.size > 0:
-        v = v[0].item()
-
-        # new_graph = np.delete(np.delete(graph, v, axis=0), v, axis=1)
+    v = find_vertex_without_neighbours(graph)
+    # print("v = ", v)
+    if v:
         GV_v = graph.copy()
         GV_v[v] = -1
-        GV_v[...,v] = -1
+        GV_v[..., v] = -1
         return 1 + r0(GV_v)
 
     # Find vertex with maximum degree
-    u = np.argmax(np.sum(graph))
+    u = find_vertex_with_max_degree(graph)
     neighbours = np.append(np.where(graph[u] == 1), u)
-    print("u = ", u)
-    print("neighbours = ", neighbours)
+    # print("u = ", u)
+    # print("neighbours = ", neighbours)
 
     GV_Nu = graph.copy()
     GV_Nu[neighbours] = -1
-    GV_Nu[...,neighbours] = -1
+    GV_Nu[..., neighbours] = -1
 
     GV_u = graph.copy()
     GV_u[u] = -1
-    GV_u[...,u] = -1
+    GV_u[..., u] = -1
     return max(1 + r0(GV_Nu), r0(GV_u))
 
 
